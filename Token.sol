b521d50d5870362a66792c97bf7a4efbbf160f27
kissebaaz <129421527+kissebaaz@users.noreply.github.com>
2023-03-30 22:37:55 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -18,11 +18,23 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
+    string public tokenname = "SATYAM"; 
+    string public tokenAbbrv = "STM"; 
+    uint public totalSupply = 0;
 
     // mapping variable here
+    mapping(address => uint) public balances;
 
     // mint function
-
+    function mint (address _address, uint _value) public {
+        totalSupply += _value; 
+        balances[_address] += _value;
+    }
     // burn function
-
+    function burn (address _address, uint _value) public { 
+        if (balances[_address] >= _value) {
+            totalSupply -= _value; 
+            balances[_address] -= _value;
+        }
+    }
 }
